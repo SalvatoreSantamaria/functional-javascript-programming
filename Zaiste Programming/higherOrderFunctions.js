16ee5c8c4c84c304f7f9082af053943b0bfcee23
Salvatore Santamaria <salvatore.santamaria@gopuff.com>
2021-02-23 15:02:54 -0800

    added higher order functions overview

---
+++ Zaiste Programming/higherOrderFunctions.js
@@ -0,0 +1,36 @@
+// Higher order functions and functions that can be passed as input to other functions
+// or
+// returned from other functions as output. 
+
+
+
+
+
+//returning functions as output
+const profile = (fn) => {
+    return (...args) => {
+        console.log(`Started at ${Date()}`)
+        const result = fn(...args);
+        console.log(`Computing ${result}`)
+        console.log(`Ended at ${Date()}`)
+    }
+}
+
+const compute = (x, y) => x + y;
+profile(compute)(2,3)
+
+
+
+
+
+//creating abstractions with functions
+const isLargerThan = value => {
+    return (number) => {
+        return number > value;
+    }
+}
+
+const isLargerThan10 = isLargerThan(10)
+const isLargerThan100 = isLargerThan(100)
+const result = isLargerThan10(14)
+console.log(result)
